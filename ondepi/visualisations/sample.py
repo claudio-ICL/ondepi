25cacd8b14ebfd391b8f8e16e2497ca778344829
Claudio Bellani <c.bellani17@imperial.ac.uk>
2021-06-18 15:30:01 +0200

    Visualisation of filter
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   applications/filter.ipynb
    	modified:   visualisations/sample.py

--- ondepi/visualisations/sample.py
+++ ondepi/visualisations/sample.py
@@ -28,6 +28,25 @@ def plot_reconstructed_volumes(df, ax=None, time_range=None, std_size=100, **kwa
     ax.bar(time, volumes, **kwargs)
     ax.set_title(
         f'Level volumes reconstructed from queue state')
-    ax.set_xlabel('time in microseconds')
+    ax.set_xlabel('time in seconds')
     ax.set_ylabel('volume')
+    return ax
+
+
+def plot_df_detection(df, ax=None, time_range=None, std_size=100, **kwargs):
+    df['time'] = df['time sample']
+    df, ax = __init__(df, ax, time_range)
+    time = df['time'].values
+    volumes = std_size * df['state'].values
+    predictions = std_size * df['predictor'].values
+    kwargs['width'] = kwargs.get('width', 7.5e-1)
+    kwargs['label'] = kwargs.get('label', 'Queue size')
+    ax.bar(time, volumes, **kwargs)
+    ax.plot(time, predictions, color='green', linewidth=3.0,
+            linestyle='--', label='Expected value')
+    ax.set_title(
+        f'Level volumes reconstructed from queue state and their expected values')
+    ax.set_xlabel('time in seconds')
+    ax.set_ylabel('volume')
+    ax.legend()
     return ax
